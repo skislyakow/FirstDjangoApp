47533cd16a141239aca3f5e68f7ad181d1134c2d
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-06-19 15:32:35 +0300

    Writing your first Django app, part 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,10 @@
+from typing import Any
+
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -11,12 +15,17 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
